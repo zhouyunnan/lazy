72c9657c8667eac9368521b01e59a2efabf947ea
zhouyn <164041601@qq.com>
2019-11-21 15:08:55 +0800

    订单页

--- kk/pages/Dingdan/list.vue
+++ kk/pages/Dingdan/list.vue
@@ -1,3 +1,417 @@
 <template>
-    <view>1</view>
+  <view>
+    <view class="type_">
+      <view class="box" @click="changedis(0)">
+        <view class="msg" :class="{boddr:nodis == 0}">待配送</view>
+      </view>
+      <view class="box" @click="changedis(1)">
+        <view class="msg" :class="{boddr:nodis == 1}">派送中</view>
+      </view>
+      <view class="box" @click="changedis(2)">
+        <view class="msg" :class="{boddr:nodis == 2}">已完成</view>
+      </view>
+    </view>
+    <view class="dd_">
+      <view v-show="nodis == 0">
+        <view class="box_">
+          <view class="w">
+            <view class="tt">
+              <view class="fle" style="opacity:1">
+                <view class="time">10.01 - 10:01</view>
+                <view class="zt1">已接单</view>
+              </view>
+            </view>
+            <view class="dizhi">成都市武侯区武侯大道金楠国际2-3804</view>
+            <view class="content">顺丰-20-30-10</view>
+          </view>
+        </view>
+        <view class="box_">
+          <view class="w">
+            <view class="tt">
+              <view class="fle" style="opacity:1">
+                <view class="time">10.01 - 10:01</view>
+                <view class="zt1">已接单</view>
+              </view>
+            </view>
+            <view class="dizhi">成都市武侯区武侯大道金楠国际2-3804</view>
+            <view
+              class="content"
+            >中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！</view>
+          </view>
+        </view>
+        <view class="box_">
+          <view class="w">
+            <view class="tt">
+              <view class="fle" style="opacity:1">
+                <view class="time">10.01 - 10:01</view>
+                <view class="zt1">已接单</view>
+              </view>
+            </view>
+            <view class="dizhi">成都市武侯区武侯大道金楠国际2-3804</view>
+            <view
+              class="content"
+            >中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！</view>
+          </view>
+        </view>
+      </view>
+      <view v-show="nodis == 1">
+        <view class="box_">
+          <view class="w">
+            <view class="tt">
+              <view class="fle" style="opacity:0.5">
+                <view class="time">10.01 - 10:01</view>
+                <view class="zt1">已接单</view>
+              </view>
+              <view class="fle" style="opacity:1">
+                <view class="time">10:31</view>
+                <view class="zt2">派送中</view>
+              </view>
+            </view>
+            <view class="dizhi">成都市武侯区武侯大道金楠国际2-3804</view>
+            <view
+              class="content"
+            >中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！</view>
+          </view>
+        </view>
+        <view class="box_">
+          <view class="w">
+            <view class="tt">
+              <view class="fle" style="opacity:0.5">
+                <view class="time">10.01 - 10:01</view>
+                <view class="zt1">已接单</view>
+              </view>
+              <view class="fle" style="opacity:1">
+                <view class="time">10:31</view>
+                <view class="zt2">派送中</view>
+              </view>
+            </view>
+            <view class="dizhi">成都市武侯区武侯大道金楠国际2-3804</view>
+            <view
+              class="content"
+            >中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！</view>
+          </view>
+        </view>
+        <view class="box_">
+          <view class="w">
+            <view class="tt">
+              <view class="fle" style="opacity:0.5">
+                <view class="time">10.01 - 10:01</view>
+                <view class="zt1">已接单</view>
+              </view>
+              <view class="fle" style="opacity:1">
+                <view class="time">10:31</view>
+                <view class="zt2">派送中</view>
+              </view>
+            </view>
+            <view class="dizhi">成都市武侯区武侯大道金楠国际2-3804</view>
+            <view
+              class="content"
+            >中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！</view>
+          </view>
+        </view>
+        <view class="box_">
+          <view class="w">
+            <view class="tt">
+              <view class="fle" style="opacity:0.5">
+                <view class="time">10.01 - 10:01</view>
+                <view class="zt1">已接单</view>
+              </view>
+              <view class="fle" style="opacity:1">
+                <view class="time">10:31</view>
+                <view class="zt2">派送中</view>
+              </view>
+            </view>
+            <view class="dizhi">成都市武侯区武侯大道金楠国际2-3804</view>
+            <view
+              class="content"
+            >中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！</view>
+          </view>
+        </view>
+      </view>
+      <view v-show="nodis == 2">
+        <view class="box_">
+          <view class="w">
+            <view class="tt">
+              <view class="fle" style="opacity:0.3">
+                <view class="time">10.01 - 10:01</view>
+                <view class="zt1">已接单</view>
+              </view>
+              <view class="fle" style="opacity:0.5">
+                <view class="time">10:31</view>
+                <view class="zt2">派送中</view>
+              </view>
+              <view class="fle" style="opacity:1">
+                <view class="time">11:01</view>
+                <view class="zt3">已完成</view>
+              </view>
+            </view>
+            <view class="dizhi">成都市武侯区武侯大道金楠国际2-3804</view>
+            <view
+              class="content"
+            >中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！</view>
+          </view>
+        </view>
+        <view class="box_">
+          <view class="w">
+            <view class="tt">
+              <view class="fle" style="opacity:0.3">
+                <view class="time">10.01 - 10:01</view>
+                <view class="zt1">已接单</view>
+              </view>
+              <view class="fle" style="opacity:0.5">
+                <view class="time">10:31</view>
+                <view class="zt2">派送中</view>
+              </view>
+              <view class="fle" style="opacity:1">
+                <view class="time">11:01</view>
+                <view class="zt3">已完成</view>
+              </view>
+            </view>
+            <view class="dizhi">成都市武侯区武侯大道金楠国际2-3804</view>
+            <view
+              class="content"
+            >中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！</view>
+          </view>
+        </view>
+        <view class="box_">
+          <view class="w">
+            <view class="tt">
+              <view class="fle" style="opacity:0.3">
+                <view class="time">10.01 - 10:01</view>
+                <view class="zt1">已接单</view>
+              </view>
+              <view class="fle" style="opacity:0.5">
+                <view class="time">10:31</view>
+                <view class="zt2">派送中</view>
+              </view>
+              <view class="fle" style="opacity:1">
+                <view class="time">11:01</view>
+                <view class="zt3">已完成</view>
+              </view>
+            </view>
+            <view class="dizhi">成都市武侯区武侯大道金楠国际2-3804</view>
+            <view
+              class="content"
+            >中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！</view>
+          </view>
+        </view>
+        <view class="box_">
+          <view class="w">
+            <view class="tt">
+              <view class="fle" style="opacity:0.3">
+                <view class="time">10.01 - 10:01</view>
+                <view class="zt1">已接单</view>
+              </view>
+              <view class="fle" style="opacity:0.5">
+                <view class="time">10:31</view>
+                <view class="zt2">派送中</view>
+              </view>
+              <view class="fle" style="opacity:1">
+                <view class="time">11:01</view>
+                <view class="zt3">已完成</view>
+              </view>
+            </view>
+            <view class="dizhi">成都市武侯区武侯大道金楠国际2-3804</view>
+            <view
+              class="content"
+            >中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！</view>
+          </view>
+        </view>
+        <view class="box_">
+          <view class="w">
+            <view class="tt">
+              <view class="fle" style="opacity:0.3">
+                <view class="time">10.01 - 10:01</view>
+                <view class="zt1">已接单</view>
+              </view>
+              <view class="fle" style="opacity:0.5">
+                <view class="time">10:31</view>
+                <view class="zt2">派送中</view>
+              </view>
+              <view class="fle" style="opacity:1">
+                <view class="time">11:01</view>
+                <view class="zt3">已完成</view>
+              </view>
+            </view>
+            <view class="dizhi">成都市武侯区武侯大道金楠国际2-3804</view>
+            <view
+              class="content"
+            >中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！中国的多端开发时代演进，促使跨端开发领域必然产生新的平台巨头，加入DCloud，一起创造未来！</view>
+          </view>
+        </view>
+        <view class="loading">{{loading}}</view>
+      </view>
+    </view>
+  </view>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      nodis: 0,
+      loading: "上拉加载更多..."
+    };
+  },
+  methods: {
+    changedis(val) {
+      this.nodis = val;
+    }
+  },
+  watch: {
+    nodis() {}
+  },
+  onPullDownRefresh() {
+    console.log("refresh");
+    setTimeout(function() {
+      uni.stopPullDownRefresh();
+    }, 1000);
+  },
+  onReachBottom() {
+    let thiz = this;
+    thiz.loading = "加载中...";
+    setTimeout(function() {
+      thiz.loading = "没有了";
+    }, 3000);
+  }
+};
+</script>
+<style lang="scss" scoped>
+@mixin w_ {
+  width: 750rpx;
+  display: flex;
+  .w {
+    flex: 1;
+    margin: 0rpx 20rpx;
+  }
+
+  font-size: 32rpx !important;
+  color: #474747;
+}
+.type_ {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  z-index: 9;
+  width: 100vw;
+  height: 90rpx;
+  background: white;
+  display: flex;
+  .box {
+    flex: 1;
+    position: relative;
+    .msg {
+      position: absolute;
+      bottom: 0rpx;
+      left: 50%;
+      height: 70rpx;
+      line-height: 70rpx;
+      font-size: 32rpx;
+      transform: translateX(-50%);
+      color: #818181;
+    }
+    .boddr {
+      border-bottom: 4rpx solid #474747;
+      color: #474747;
+    }
+  }
+}
+.dd_ {
+  margin-top: 110rpx;
+  .box_ {
+    @include w_;
+    margin-top: 20rpx;
+    .w {
+      background: white;
+      border-radius: 10rpx;
+      .tt {
+        width: calc(100% - 20rpx);
+        padding: 20rpx 0rpx;
+        margin: 0 auto;
+        border-bottom: 1px solid #f0f0f0;
+        font-size: 28rpx;
+        .fle {
+          display: flex;
+          padding: 5rpx;
+          .time {
+            flex: 1;
+            color: #818181;
+          }
+
+          .zt1 {
+            color: #474747;
+            position: relative;
+            margin-right: 35rpx;
+            font-size: 28rpx;
+            &::after {
+              position: absolute;
+              content: "";
+              background: url("../../static/images/jiedan.png") no-repeat;
+              width: 30rpx;
+              height: 30rpx;
+              background-size: 100% 100%;
+              top: 50%;
+              margin-top: -15rpx;
+              left: 100%;
+              margin-left: 5rpx;
+            }
+          }
+          .zt2 {
+            @extend .zt1;
+            &::after {
+              position: absolute;
+              content: "";
+              background: url("../../static/images/diandongche.png") no-repeat;
+              width: 28rpx;
+              height: 28rpx;
+              background-size: 100% 100%;
+              top: 50%;
+              margin-top: -14rpx;
+              left: 100%;
+              margin-left: 5rpx;
+              opacity: 0.8;
+            }
+          }
+          .zt3 {
+            @extend .zt1;
+            &::after {
+              position: absolute;
+              content: "";
+              background: url("../../static/images/woshou.png") no-repeat;
+              width: 30rpx;
+              height: 30rpx;
+              background-size: 100% 100%;
+              top: 50%;
+              margin-top: -15rpx;
+              left: 100%;
+              margin-left: 5rpx;
+            }
+          }
+        }
+      }
+      .dizhi {
+        width: calc(100% - 20rpx);
+        margin: 15rpx auto;
+        font-size: 32rpx;
+        color: #474747;
+      }
+      .content {
+        width: calc(100% - 20rpx);
+        margin: 15rpx auto;
+        font-size: 28rpx;
+        padding-bottom: 10rpx;
+        color: #818181;
+        line-height: 1.2;
+        text-align: justify;
+      }
+    }
+  }
+}
+.loading {
+  font-size: 28rpx;
+  color: #818181;
+  text-align: center;
+  width: 100vw;
+  line-height: 60rpx;
+  padding: 60rpx 0rpx;
+}
+</style>
--- kk/pages/index/index.vue
+++ kk/pages/index/index.vue
@@ -3,9 +3,22 @@
     <view class="top_">
       <view class="school" @click="linkschool_()">海南热带海洋学院</view>
     </view>
+    <view class="yysj">
+      <view class="w">接单时间：08.00 - 12.30</view>
+    </view>
+    <view class="lxdh">
+      <view class="w">
+        联系电话：
+        <text @click="call()">17600664494</text>
+      </view>
+    </view>
     <view class="gg_">
-      <view>公告</view>
-      <text>abbar 的默认高度，在不同平台不一样。App端的默认高度在HBuilderX 2.3.4起从56px调整为50px，与H5端统一。开发者也可以自行设定高度，调回56px</text>
+      <view class="tit">联盟公示</view>
+      <view class="msg">
+        abbar 的默认高度，在不同平台不一样。App端的默view
+        认高度在HBuilderX 2.3.4起从56px调整为50px，与H5端统
+        一。开发者也可以自行设定高度，调回56px
+      </view>
     </view>
     <view class="xiadan">
       <text>立即下单</text>
@@ -19,12 +32,43 @@ export default {
       uni.navigateTo({
         url: "/pages/school/xuanzheschool"
       });
+    },
+    call() {
+      uni.makePhoneCall({
+        phoneNumber: "17600664494" //仅为示例
+      });
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+@mixin w_ {
+  width: 750rpx;
+  display: flex;
+  .w {
+    flex: 1;
+    margin: 0rpx 20rpx;
+  }
+  font-size: 32rpx !important;
+  color: #474747;
+}
+.yysj,
+.lxdh {
+  @include w_;
+  .w {
+    background: white;
+    line-height: 60rpx;
+    font-size: 32rpx;
+    border-radius: 10rpx;
+    margin-top: 20rpx;
+    text-indent: 10rpx;
+  }
+  text {
+    color: rgb(2, 91, 163);
+  }
+}
+
 .top_ {
   width: 750rpx;
   background: white;
@@ -53,26 +97,30 @@ export default {
     }
   }
 }
-.gg_{
-    width: 710rpx;
+.gg_ {
+  width: 710rpx;
+  margin: 20rpx auto;
+  min-height: 200rpx;
+  background: white;
+  border-radius: 10rpx;
+  font-size: 32rpx;
+  color: #474747;
+  .tit {
+    width: calc(100% - 20rpx);
     margin: 20rpx auto;
-    min-height: 200rpx;
-    background: white;
-    border-radius: 10rpx;
-    font-size: 32rpx;
-    color: #474747;
-    view{
-      width: calc(100% - 20rpx);
-      margin: 20rpx auto;
-      float: left;
-      text-align: center
-    }
-    text{
-      width: calc(100% - 20rpx);
-      margin: 0 auto;
-    }
+    float: left;
+    text-align: center;
+  }
+  .msg {
+    width: calc(100% - 20rpx);
+    margin: 0 auto;
+    padding-bottom: 30rpx;
+    line-height: 1.5;
+    text-align: justify;
+  }
 }
 .xiadan {
+  position: fixed;
   width: 200rpx;
   height: 200rpx;
   border-radius: 50%;
@@ -80,8 +128,9 @@ export default {
   line-height: 200rpx;
   color: white;
   font-size: 34rpx;
-  background: #ff507d;
-  margin: 0 auto;
-  margin-top: 40rpx;
+  background: radial-gradient(circle at center, #ff507d, #ffffff);
+  background: radial-gradient(circle at top, #ff507d, #ff81a0, #e90941);
+  bottom: 150rpx;
+  left: 275rpx;
 }
 </style>
