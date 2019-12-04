dba194d51bb1fc14b0148015c15a3a4901b0e8b5
zhouyn <164041601@qq.com>
2019-12-04 17:30:50 +0800

    1

--- kk/App.vue
+++ kk/App.vue
@@ -8,6 +8,21 @@ export default {
 	},
 	onHide: function() {
 		console.log('App Hide');
+	},
+	onError: function(err) {
+		var json = JSON.stringify(err);  
+		uni.request({
+			method: 'GET',
+			url: this.myconfig.url + 'qq.php/home/error/add',
+			data: {
+				"e":json,
+			
+			},
+			header: {
+				'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+			},
+			success: res => {}
+		});
 	}
 };
 </script>
